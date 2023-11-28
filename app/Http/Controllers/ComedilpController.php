4a1a6f3d52150c6cfa08b242f5fead904ed19f3a
Alexis-Chata <alexizz.19.ac@gmail.com>
2023-11-27 21:23:11 -0500

    Comedilp

---
+++ app/Http/Controllers/ComedilpController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comedilp;
+use App\Http\Requests\StoreComedilpRequest;
+use App\Http\Requests\UpdateComedilpRequest;
+
+class ComedilpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreComedilpRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Comedilp $comedilp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Comedilp $comedilp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateComedilpRequest $request, Comedilp $comedilp)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Comedilp $comedilp)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_11_28_022034_create_comedilps_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comedilps', function (Blueprint $table) {
+            $table->id();
+            $table->char('ccia', 2); // Código de Compañia
+            $table->char('cdivi', 2); // Código de División de Negocios
+            $table->char('ccendis', 2); // Código Centro de Distribución
+            $table->char('clistpr', 3); // Código Lista de Precios
+            $table->char('ccodart', 10); // Código de Artículo
+            $table->double('qprecio', 9, 2); // Precio Artículo (incluye IGV)
+            $table->char('cuser', 10); // Nombre de Usuario
+            $table->char('cidpr', 12); // Nombre del Programa
+            $table->date('fupgr'); // Fecha de Actualización
+            $table->char('tupgr', 8); // Hora de Actualización
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comedilps');
+    }
+};

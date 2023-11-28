e4574a5e991d33a5efb47411d3fdace0bcfad702
Alexis-Chata <alexizz.19.ac@gmail.com>
2023-11-27 21:31:30 -0500

    Comedi31

---
+++ app/Http/Controllers/Comedi31Controller.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comedi31;
+use App\Http\Requests\StoreComedi31Request;
+use App\Http\Requests\UpdateComedi31Request;
+
+class Comedi31Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreComedi31Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Comedi31 $comedi31)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Comedi31 $comedi31)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateComedi31Request $request, Comedi31 $comedi31)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Comedi31 $comedi31)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_11_28_022407_create_comedi31s_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comedi31s', function (Blueprint $table) {
+            $table->id();
+            $table->char('ccia', 2); // Código de Compañia
+            $table->char('cdivi', 2); // Código de División de Negocios
+            $table->char('ccendis', 2); // Código Centro de Distribución
+            $table->char('ccli', 8); // Código de Cliente
+            $table->char('crut', 3); // Código de Ruta
+            $table->char('nsecprev', 4); // Secuencia de visita en la ruta
+            $table->char('clin', 2); // Código Línea Preventista
+            $table->char('cven', 3); // Código de Prevendedor
+            $table->char('cdiavis', 1); // Día de Visita:1:Lunes, 2:Martes, 3:Miércoles, 4:Jueves, 5:Viernes, 6:Sábado, 7:Domingo
+            $table->double('qimpvta', 12, 2); // Importe Venta de última visita
+            $table->date('femi'); // Fecha de emisión última visita
+            $table->char('cmrp', 2); // Código Motivo Rechazo Pedido (si tiene dato este campor es porque el último pedido fue rechazado por el cliente)
+            $table->char('cuser', 10); // Nombre de Usuario
+            $table->char('cidpr', 12); // Nombre del Programa
+            $table->date('fupgr'); // Fecha de Actualización
+            $table->char('tupgr', 8); // Hora de Actualización
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comedi31s');
+    }
+};

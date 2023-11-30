9a8d702d09f66ecd3dd5758b61274363240623eb
Alexis-Chata <alexizz.19.ac@gmail.com>
2023-11-30 14:22:50 -0500

    Mejoras

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Tomador de Pedidos Web'),
 
     /*
     |--------------------------------------------------------------------------
--- config/session.php
+++ config/session.php
@@ -128,7 +128,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+        Str::slug(env('APP_NAME', 'Tomador de Pedidos Web'), '_').'_session'
     ),
 
     /*

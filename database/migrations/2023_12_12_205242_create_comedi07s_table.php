2b4e222095108f518484ba40bd314edbd686f1f2
Alexis-Chata <alexizz.19.ac@gmail.com>
2023-12-14 11:19:45 -0500

    Avance

--- database/migrations/2023_12_12_205242_create_comedi07s_table.php
+++ database/migrations/2023_12_12_205242_create_comedi07s_table.php
@@ -12,8 +12,49 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('comedi07s', function (Blueprint $table) {
+            // Maestro de Clientes
             $table->id();
-            $table->timestamps();
+            $table->char('cter', 2)->default('11'); // código de territorio.
+            $table->char('creg', 2)->default('11'); // código de región.
+            $table->char('ccia', 2)->default('11'); // Código de Compañia
+            $table->char('cdivi', 2); // Código de División de Negocios
+            $table->char('ccendis', 2)->default('07'); // Código Centro de Distribución
+            $table->char('ccli', 8); // código de cliente.
+            $table->string('tnomrep', 40); // nombre del representante.
+            $table->string('tdir', 50); // dirección del cliente.
+            $table->string('tcli', 40); // nombre y apellidos o razón social.
+            $table->string('ntel', 10); // número de teléfono.
+            $table->char('le', 8); // Dni.
+            $table->char('ctipneg', 3); // tabla:'013':tipo de negocio.
+            $table->string('tdestneg', 40); // descripción tipo de negocio.
+            $table->char('ctipcli', 1); // tabla '012': tipos de cliente '1':compartido, '2':exclusivos.
+            $table->string('tdestcli', 40); // descripción de tipo de cliente.
+            $table->char('ctipseg', 4); // tabla '021': segmentos de mercado '1':minorista, '2':mayoristas.
+            $table->string('tdestseg', 40); // descripción segmento de mercado.
+            $table->char('flagact', 1); // estado del cliente '1':activo, '2':inactivo.
+            $table->string('tdesescl', 40); // descripción estado del cliente.
+            $table->string('cruc', 11); // ruc del cliente.
+            $table->date('fnac'); // fecha de nacimiento del cliente.
+            $table->char('crub', 2); // tabla '049':rubros de negocio.
+            $table->string('tdesrubr', 40); // descripción rubro de negocio.
+            $table->char('crutd', 2); // tabla '097':tipos agente Sunat.
+            $table->string('tdesrutd', 40); // descripción tipo agente Sunat.
+            $table->char('ctipco', 2); // Tabla:’083’:Tipo documento Contribuyente. ‘01’:DNI, ‘06’:RUC.
+            $table->string('tdestico', 40); // descripción Tipo documento Contribuyente.
+            $table->char('fligv', 1); // condición IGV, '1':exonerado de IGV, '2':no exonerado de IGV.
+            $table->string('tdesfigv', 40); // descripción condición IGV.
+            $table->char('tdocid', 2); // tabla '089':tipo de contribuyente, '01':persona natural, '02':persona jurídica.
+            $table->string('tdestdoc', 40); // descripción tipo de contribuyente.
+            $table->char('clistpr', 3); // tabla '055': lista de precios.
+            $table->string('tdeslpre', 40); // descripción lista de precios.
+            $table->string('cuser', 10); // Nombre de Usuario
+            $table->string('cidpr', 12); // Nombre del Programa
+            $table->date('fupgr'); // Fecha de Actualización
+            $table->time('tupgr', 8); // Hora de Actualización
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+
+            $table->unique(['ccli']);
         });
     }
 
--- database/migrations/2023_12_12_205443_create_comedi26s_table.php
+++ database/migrations/2023_12_12_205443_create_comedi26s_table.php
@@ -12,8 +12,37 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('comedi26s', function (Blueprint $table) {
+            // Maestro de Promociones
             $table->id();
-            $table->timestamps();
+            $table->char('ccia', 2)->default('11'); // Código de Compañia
+            $table->char('cdivi', 2); // Código de División de Negocios
+            $table->char('ccendis', 2)->default('07'); // Código Centro de Distribución
+            $table->char('cprom', 3); // Código de Promoción
+            $table->string('tprom', 50); // Nombre de la Promoción
+            $table->char('clin', 2)->default('03'); // tabla '011':Línea preventista.
+            $table->string('tdesclin', 40); // descripción línea preventista.
+            $table->double('qprecio', 9, 2); // Precio Artículo
+            $table->double('qpordes', 6, 2); // Porcentaje de Descuento de la Promoción.
+            $table->date('finipro'); // Fecha Inicio de la Promoción.
+            $table->date('ffinpro'); // Fecha Final de la Promoción.
+            $table->char('ctipneg', 2)->default('00'); // tabla:'013':tipo de negocio.
+            $table->string('tdestneg', 40); // descripción tipo de negocio.
+            $table->char('ctipro', 2)->default('00'); // tabla:'078':tipo promoción.
+            $table->string('tdestpro', 40); // descripción tipo promoción. '01':obsequios promocionales, '02':bonificación horizontal.
+            $table->string('ccodart1', 10); // código artículo venta.
+            $table->double('qprod1', 9, 2); // cantidad exigida de venta.
+            $table->char('cescom', 1)->default('2'); // '1':comisionable, '2':no comisionable.
+            $table->string('tdesccom', 40); // descripción tipo comisionable.
+            $table->string('ccodart2', 10); // código artículo regalo.
+            $table->double('qprod2', 9, 2); // cantidad de regalo.
+            $table->string('cuser', 10); // Nombre de Usuario
+            $table->string('cidpr', 12); // Nombre del Programa
+            $table->date('fupgr'); // Fecha de Actualización
+            $table->time('tupgr', 8); // Hora de Actualización
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+
+            $table->unique(['ccia', 'cdivi', 'ccendis', 'cprom']);
         });
     }
 
